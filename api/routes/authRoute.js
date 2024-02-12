f637cf5c2e8771d7ebcf6c4ae98e8bb54014871e
raghav <raghavattri13@gmail.com>
2024-02-12 16:17:43 +0530

    first commit

---
+++ api/routes/authRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  signin,
+  signout,
+  signup,
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+
+export default router;
